5808c7a0fb34f3d3cdd18bffc564b7f938981292
Tanujadevim <tanujadevi.muthyalapali@gmail.com>
2026-05-02 12:35:49 +0530

    Production settings for deployment

--- core/settings.py
+++ core/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -124,3 +125,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
