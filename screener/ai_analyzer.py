5e1573d07d965c43ccdac77809a8133a1a4dbbb9
Tanujadevim <tanujadevi.muthyalapali@gmail.com>
2026-05-02 13:25:47 +0530

    fix poppler and tesseract for Railway Linux

--- screener/ai_analyzer.py
+++ screener/ai_analyzer.py
@@ -5,8 +5,11 @@ import pytesseract
 from PIL import Image
 import io
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
+import shutil
+tesseract_path = shutil.which('tesseract')
+if tesseract_path:
+    pytesseract.pytesseract.tesseract_cmd = tesseract_path
+    
 def extract_text_from_pdf(pdf_file):
     try:
         text = ""
